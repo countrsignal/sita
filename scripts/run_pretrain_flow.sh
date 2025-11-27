32ff53ebd843e53d8f298fd9e31a0a74359f42af
countrsignal <dpenaher@protonmail.com>
2025-11-27 14:41:05 -0500

    train transferable flow for 10 epochs

--- scripts/run_pretrain_flow.sh
+++ scripts/run_pretrain_flow.sh
@@ -45,7 +45,7 @@ echo "WANDB_ENTITY='${USERNAME}'" > .env
 ## Launch Training Script ##
 ############################
 echo "Launching training script..."
-python "$PRETRAIN_SCRIPT" experiment=pretrain_transferable_flow_temp trainer.max_epochs=500
+python "$PRETRAIN_SCRIPT" experiment=pretrain_transferable_flow_temp trainer.max_epochs=10
 
 echo "Training COMPLETE."
 exit 0
